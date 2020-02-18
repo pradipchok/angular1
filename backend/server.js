456cef21a577539844ada14fc0bcf339ce1e48f8
JP <contactjaypal@gmail.com>
2020-02-18 13:53:07 +0530

    hello world in browser

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ var port = 3000;
 
 var app = express();
 
+app.get('/', (req, res) => {
+    res.write('<h1>Hello World</h1>');
+});
 
 app.listen(port, function () {
     console.log('Server started on port ' + port);
